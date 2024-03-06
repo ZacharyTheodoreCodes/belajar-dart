41f390cf89c74e446ba9feaddf5d4f66ee58bce2
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-06 20:46:50 +0700

    solid principles

---
+++ bin/solid_demo.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  /**
+   * S: single responsibility
+   * O: open principle
+   * L: liskov substitution
+   * I: Interface segregation
+   * D: Dependency Inversion
+   * 
+   */
+}
