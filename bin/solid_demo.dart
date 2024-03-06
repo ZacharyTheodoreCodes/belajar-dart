3ec1b7553b88fac86b5a829f5f071922c6658dd6
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-06 22:07:46 +0700

    commit

--- bin/solid_demo.dart
+++ bin/solid_demo.dart
@@ -1,10 +1,9 @@
 void main(List<String> args) {
-  /**
+  /* 
    * S: single responsibility
    * O: open principle
    * L: liskov substitution
    * I: Interface segregation
    * D: Dependency Inversion
-   * 
    */
 }
