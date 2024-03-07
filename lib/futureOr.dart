dd8312b49aa52c26de4402c88931f5e640fb2f3f
unknown <BNI@WDL-67643-NB.hq.bni.co.id>
2024-03-07 15:59:47 +0700

    futureOr

--- lib/futureOr.dart
+++ lib/futureOr.dart
@@ -4,8 +4,19 @@
  * implements -> pake semua methodnya, di-override
  * Extends -> class inheritance
  */
+
+import 'dart:async';
+
+void main(List<String> args) async {
+  User user = User();
+  UserManager userManager = UserManager(FirebaseStorage());
+
+  print(await userManager.getUserAge(user));
+}
+
+
 class User {
- 
+ int age = 0;
 }
 
 class UserManager{
@@ -16,21 +27,34 @@ class UserManager{
   void saveUserData(User user, int option){
     dataStorage.saveData(user);
   }
- 
+  
+  FutureOr<int> getUserAge(User user) async{
+    return await dataStorage.getUserAge(user);
+  }
 }
 
 abstract class IDDataStorage{
   void saveData(User user);
-  
+  FutureOr<int> getUserAge(User user);
 }
+
 
 class FirebaseStorage implements IDDataStorage{
   @override
   void saveData(User user){}
+
+  @override
+  FutureOr<int> getUserAge(User user) async{
+    await Future.delayed(Duration(seconds: 1));
+    return 20;
+  }
 }
 
 class LocalStorage implements IDDataStorage{
-    @override
+  @override
   void saveData(User user){}
+
+  @override
+  int getUserAge(User user) => 10;
 }
 
