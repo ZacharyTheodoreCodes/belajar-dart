795a29b8edf37b1ca7a007276652166eaadc69de
unknown <BNI@WDL-67643-NB.hq.bni.co.id>
2024-03-07 17:17:32 +0700

    dart 2.17

--- bin/dart217.dart
+++ bin/dart217.dart
@@ -1,8 +1,17 @@
 //Named Parameter
+import 'dart:html';
+
 void main(List<String> args) {
   //positional parameter: urutannya harus sama
   //optional parameter: fleksibel urutannya
   User u = User(200, 'Joko', age: 5, fullName: 'Joko Subandi');
+  //declare enumnya: pake dot.
+  UserAuthException a = UserAuthException.invalidPassword;
+
+  print(a); //sama dengan a.toString()
+  print(a.code);
+  print(a.description);
+
 }
 
 enum AdministratorType{
@@ -29,3 +38,33 @@ class Administrator extends User{
 }
 
 //Enhanced Enum
+/**
+ * Before: kalau mau bikin property di anaknya enum, harus bikin extension dan didalamnya ada getter
+ */
+enum UserAuthException{
+  invalidPassword(100,'Wrong Password'),
+  invalidUsername(200,'Username does not exist');
+
+  final int code;
+  final String description;
+
+  const UserAuthException(this.code, this.description);
+
+  double get exceptionCodeInDouble => code.toDouble();
+
+  @override 
+  String toString() => '$name is one of user auth exceptions.';
+}
+
+
+
+//before
+// extension UserAuthExceptionExtension on UserAuthException{
+//   String get description{
+//     if (name == 'invalidPassowrd'){
+//       return 'Wrong Password';
+//     }else {
+//       return 'Username does not exist';
+//     }
+//   }
+// }
